5b14e8c622962976bd31a4528cd7c09d7c530e68
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-09-15 17:15:58 +0200

    Updated SeeDice

--- Program.cs
+++ Program.cs
@@ -1,16 +1,40 @@
 ﻿Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
+int total = roll1 + roll2 + roll3;
 
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-Console.WriteLine(largerValue);
-System.Ma
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}f
